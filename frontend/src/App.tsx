b79d6bbee127f54060357029717cbc5ffd152fd1
BarryUpkins <pk1st.backup@gmail.com>
2022-01-11 16:52:45 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import NavBar from 'components/navbar';
 import React from 'react';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from './pages/listing';
+import Form from './pages/form';
 
 function App() {
   return (
-    <NavBar />
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 
   );
 }
