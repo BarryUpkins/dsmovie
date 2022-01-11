d9d91145e2ff90d93ec68fd2c245ffe84ca2262d
BarryUpkins <pk1st.backup@gmail.com>
2022-01-11 17:36:34 -0300

    Pagination layout

---
+++ frontend/src/components/pagination/index.tsx
@@ -0,0 +1,31 @@
+import { ReactComponent as Arrow } from "assets/img/Arrow.svg";
+//import { ReactComponent as Arrow } from "../../assets/img/Arrow.svg";
+import "./styles.css";
+
+function Pagination(){
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
+    return(
+        <>
+            <div className="dsmovie-pagination-container">
+                <div className="dsmovie-pagination-box">
+                    <button className="dsmovie-pagination-button" disabled={true} >
+                        <Arrow />
+                    </button>
+                    <p>{`${1} de ${3}`}</p>
+                    <button className="dsmovie-pagination-button" disabled={false} >
+                        <Arrow className="dsmovie-flip-horizontal" />
+                    </button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Pagination;
