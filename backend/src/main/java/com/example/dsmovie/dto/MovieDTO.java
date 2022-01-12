6b293ca82c8d0a51eac41aae9347bb6b9296a822
BarryUpkins <pk1st.backup@gmail.com>
2022-01-12 19:54:11 -0300

    Find movies

--- backend/src/main/java/com/example/dsmovie/dto/MovieDTO.java
+++ backend/src/main/java/com/example/dsmovie/dto/MovieDTO.java
@@ -1,27 +1,30 @@
-package com.example.dsmovie.entities;
+package com.example.dsmovie.dto;
 
-import javax.persistence.*;
+import com.example.dsmovie.entities.Movie;
 
-@Entity
-@Table(name = "tb_movie" )
-public class Movie {
-    @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY )
+public class MovieDTO {
     private Long id;
     private String title;
     private Double score;
     private Integer count;
     private String image;
 
+    public MovieDTO() { }
 
-    public Movie() { }
-
-    public Movie(Long id, String title, Double score, Integer count, String image) {
+    public MovieDTO(Long id, String title, Double score, Integer count, String image) {
         this.id = id;
         this.title = title;
         this.score = score;
         this.count = count;
         this.image = image;
+    }
+
+    public MovieDTO( Movie movie ){
+        id = movie.getId();
+        title = movie.getTitle();
+        score = movie.getScore();
+        count = movie.getCount();
+        image = getImage();
     }
 
     public Long getId() {
